31aff55d730f3a06eb87e7f2ea2e5af9e53a2c3e
Maria Mendeleva <mvmendeleva38@gmail.com>
2021-12-20 14:00:16 +0200

    Moved try/catch/finally outside steps

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,31 +13,43 @@ node {
         git branch: 'main', url: 'https://github.com/MashaM38/restful-web-services'
     }
 
-    try {
-        stage('Build') {
-            withEnv(["MVN_HOME=$mvnHome"]){
-                sh """${MVN_HOME}/bin/mvn clean test surefire-report:report -P${PROFILE}"""
-                echo """RESULT: ${currentBuild.currentResult} """
-            }
+    stage('Build') {
+        withEnv(["MVN_HOME=$mvnHome"]) {
+            sh """${MVN_HOME}/bin/mvn clean test surefire-report:report -P${PROFILE}"""
+            echo """RESULT: ${currentBuild.currentResult} """
         }
-    } catch(e) {
+    }
+    stage('Results') {
+        echo """Build completed. Cuurent build result is: ${currentBuild.result} """
+        junit allowEmptyResults: true, testResults: 'target/surefire-reports/*.xml'
+        publishHTML([allowMissing         : true,
+                     alwaysLinkToLastBuild: false,
+                     keepAll              : true,
+                     reportDir            : 'target/site',
+                     reportFiles          : 'surefire-report.html',
+                     reportName           : 'HTML Report',
+                     reportTitles         : 'Test Results',
+                     keppAll              : true])
+    }
+    try {
+        findText(textFinders: [textFinder(alsoCheckConsoleOutput: true, regexp: 'There are test failures')])
+    } catch (e) {
         currentBuild.result = "FAILURE"
         echo """Build run result is: ${currentBuild.currentResult} """
-    }
-    finally {
-        stage('Results') {
-            findText(textFinders: [textFinder(alsoCheckConsoleOutput: true, regexp: 'There are test failures')])
-            echo """Build completed. Cuurent build result is: ${currentBuild.result} """
-            junit allowEmptyResults:true, testResults:'target/surefire-reports/*.xml'
-            publishHTML([allowMissing:true,
-                         alwaysLinkToLastBuild:false,
-                         keepAll:true,
-                         reportDir:'target/site',
-                         reportFiles:'surefire-report.html',
-                         reportName:'HTML Report',
-                         reportTitles:'Test Results',
-                         keppAll:true])
-        }
+    } finally {
+        sendMail()
     }
     cleanWs()
+}
+
+def sendMail() {
+    def recipient = 'XXX@testEmail.com'
+
+    echo "Sending email notification"
+    emailext (to: recipient,
+            subject: "RESULT build: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
+            body: """<p>STATUS: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+            <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""",
+            recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
 }
